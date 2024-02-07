1408690e48da68c90fa5f3c3c251d5def9e5eb50
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-02-07 22:34:43 +0600

    HW

--- class.py
+++ class.py
@@ -1,14 +1,46 @@
 import json
+
+
 class User:
     def __init__(self, name_user, email_user, password_user):
         self.name_user = name_user
         self.email_user = email_user
         self.password_user = password_user
     def add_json(self):
+        print("ТРЕБУЕТСЯ РЕГИСТРАЦИЯ!", "\n")
+        self.name_user = input("Введите своё имя: ")
+        self.email_user = input("Введите свою почту: ")
+        self.password_user = input("Создайте пароль : ")
         data = {'name': self.name_user, 'email': self.email_user, 'password': self.password_user}
         with open('data_file.json', 'a') as file:
             json.dump(data, file)
+            file.write("\n")
+        print("ВЫ ЗАРЕГИСТРИРОВАЛИСЬ!!!", "\n")
+        c = input(f"Хотите сменить пароль??? (да/нет): ")
+        if c == "да":
+            Change().change_password(self.password_user)
+        else:
+            print("дем дальше")
 
-User('max', 'fff@ya.ru', "123").add_json()
+
+
+class Change:
+    def change_password(self, password_user):
+        self.password_user = password_user
+        print(f'Ваш текущий пароль: {self.password_user}')
+        self.password_user = input("Введите новый пароль: ")
+        # перезаписываем JSON
+
+
+
+
+
+
+
+
+
+
+
+User("name_user", 'email_user', "password_use").add_json()
 
 
