a5c48a118c920b7f57ed4fcb6a9f5fea019c27ec
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-02-10 21:17:07 +0600

    Решил вопрос с видимостью data

--- class.py
+++ class.py
@@ -5,29 +5,42 @@ class User:
         self.email_user = email_user
         self.password_user = password_user
     def add_json(self):
+        global data
         print("ТРЕБУЕТСЯ РЕГИСТРАЦИЯ!", "\n")
         self.name_user = input("Введите своё имя: ")
         self.email_user = input("Введите свою почту: ")
         self.password_user = input("Создайте пароль : ")
         data = {'name': self.name_user, 'email': self.email_user, 'password': self.password_user}
-        with open('data_file.json', 'a') as file:
+        with open('data_file.json', 'a', encoding='utf-8') as file:
             json.dump(data, file)
             file.write("\n")
         print("ВЫ ЗАРЕГИСТРИРОВАЛИСЬ!!!", "\n")
-        c = input(f"Хотите сменить пароль??? (да/нет): ")
-        if c == "да":
+        question_change = input(f"Хотите сменить пароль??? (да/нет): ")
+        if question_change == "да" or question_change == "yes" or question_change == "y":
             Change().change_password(self.password_user)
         else:
-            print("дем дальше")
-
+            pass
 
 
 class Change:
     def change_password(self, password_user):
+        global file
         self.password_user = password_user
         print(f'Ваш текущий пароль: {self.password_user}')
-        self.password_user = input("Введите новый пароль: ")
-        # перезаписываем JSON
+        print(data['password'])
+        data['password'] = 1
+        print(data['password'])
+
+
+
+
+        # change_password = input("Введите новый пароль: ")
+        # data =
+        # data.update('password', change_password)
+
+        # with open('data_file.json', 'a') as file:
+        #     json.dump(data, file)
+        #     file.write("\n")
 
 
 
