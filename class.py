7c62f5bee16dd37105002215cbafb09d7013a9de
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-02-11 23:11:40 +0600

    Не меняет пароль

--- class.py
+++ class.py
@@ -13,41 +13,43 @@ class User:
         self.email_user = input("Введите свою почту: ")
         self.password_user = input("Создайте пароль : ")
         data = [{'name': self.name_user, 'email': self.email_user, 'password': self.password_user}]
-        with open('data_file.json', 'a', encoding='utf-8') as file:
+        with open('data_file.json', 'a+', encoding='utf-8') as file:
             json.dump(data, file)
             file.write("\n")
         print("ВЫ ЗАРЕГИСТРИРОВАЛИСЬ!", "\n")
         question_change = input(f"Хотите сменить пароль??? (да/нет): ")
         print("\n")
         if question_change == "да" or question_change == "yes" or question_change == "y" or question_change == "+":
-            Change().change_password(self.password_user)
+            Change().change_password(self.name_user, self.email_user, self.password_user)
         else:
             Authentication().authentication_user(self.name_user, self.password_user)
 
 
 class Change:
-    def change_password(self, password_user):
+    def change_password(self, name_user, email_user, password_user):
+        self.name_user = name_user
+        self.email_user = email_user
         self.password_user = password_user
         print(f'Ваш текущий пароль: {self.password_user}')
         change_password = input("Введите новый пароль: ")
 
-
-        # json_data = json.loads(data)
-        # more_json_string = '{"name": "---", "email": "---", "password": "---"}'
-        # more_json_data = json.loads(more_json_string)
-        # json_data.update(more_json_data)
-
-
-        # json_data = json.loads(data)
-        # json_data[0]['name'] = 2023
+        data = [{'name': self.name_user, 'email': self.email_user, 'password': change_password}]
+        print(data[0])
+        with open('data_file.json', 'r+', encoding='utf-8') as file:
+            json.dump(data, file)
+            file.write("\n")
 
 
-        # with open("data_file.json", "r+") as f:
-        #     d = json.load(f)
-        #     d.write({
-        #         "name": 4,
-        #         "email": "Rahul Shinde",
-        #         "password": "rahul@gmail.com"})
+# a - добавляет нового пользоателя с новым паролем
+# a+ - добавляет нового пользоателя с новым паролем
+# w - перезаписывет весь файл
+# w+ - перезаписывет весь файл
+# r - выдет ошибку при смене пароля
+# r+ - добавляет нового пользоателя с новым паролем
+
+
+
+
 
 
 
